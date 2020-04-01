c791763a79889d43def706b0290e5c9f193f9f2e
Kim-Ly Le <kle@pmc.int>
2020-04-01 13:56:56 +0200

    Implemented google authentication with firebase, deployed to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -41,6 +41,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
       { path: 'shopping-cart', component: ShoppingCartComponent },
       { path: 'check-out', component: CheckOutComponent },
       { path: 'order-success', component: OrderSuccessComponent },
+      { path: 'my/orders', component: OrdersComponent },
       { path: 'login', component: LoginComponent },
       { path: 'admin/products', component: AdminProductsComponent },
       { path: 'admin/orders', component: AdminOrdersComponent },
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as firebase from 'firebase';
+import { AngularFireAuth } from '@angular/fire/auth';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor( private auth: AngularFireAuth ) { }
 
-  ngOnInit(): void {
+  login() {
+    this.auth.auth.signInWithRedirect( new firebase.auth.GoogleAuthProvider() );
   }
 
 }
